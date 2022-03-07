8de8f8ab549870f8872686f20b564d46486523dc
Kirill Voronin <yoursdearboy@gmail.com>
2022-03-07 23:40:14 +0300

    Created patients table

---
+++ src/components/PatientsTable.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-table striped hover :fields="fields" :items="items">
+    <template #cell(familyName)="data">
+      {{ data.item.name[0] && data.item.name[0].family }}
+    </template>
+    <template #cell(givenName)="data">
+      {{ data.item.name[0] && data.item.name[0].given.join(" ") }}
+    </template>
+  </b-table>
+</template>
+
+<script>
+import FHIR from "fhirclient"
+
+export default {
+  data() {
+    return {
+      fields: [
+        { key: "id", label: "ID" },
+        { key: "familyName", label: "Family name" },
+        { key: "givenName", label: "Given name" }
+      ],
+      items: []
+    }
+  },
+  async created() {
+    const client = FHIR.client("http://localhost:8090/fhir")
+    const res = await client.request("Patient")
+    const items = res.entry.map(e => e.resource)
+    this.items = items;
+  }
+}
+</script>
